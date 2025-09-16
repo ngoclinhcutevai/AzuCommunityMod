3e38200fa6c138e6bac4948861e727794c0fdca9
ngoclin.h <86618490+ngoclinhcutevai@users.noreply.github.com>
2025-09-16 22:34:34 +0700

    commit

--- src/main/java/com/ngoclinh/acm/AzusCommunityModDataGenerator.java
+++ src/main/java/com/ngoclinh/acm/AzusCommunityModDataGenerator.java
@@ -1,6 +1,7 @@
 package com.ngoclinh.acm;
 
 import com.ngoclinh.acm.datagen.ModBlockTagProvider;
+import com.ngoclinh.acm.datagen.ModItemTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,6 +9,8 @@ public class AzusCommunityModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
         pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
 	}
 }
--- src/main/java/com/ngoclinh/acm/datagen/ModItemTagProvider.java
+++ src/main/java/com/ngoclinh/acm/datagen/ModItemTagProvider.java
@@ -1,4 +1,25 @@
 package com.ngoclinh.acm.datagen;
 
-public class ModItemTagProvider {
+import com.ngoclinh.acm.item.ModItems;
+import com.ngoclinh.acm.util.ModTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        valueLookupBuilder(ModTags.Items.ACM_ITEMS)
+                .add(ModItems.DISC_BLUE)
+                .add(ModItems.DISC_CLOUDS)
+                .add(ModItems.DISC_HORIZON)
+                .add(ModItems.ECLIPSITE_DUST)
+                .add(ModItems.ECLIPSITE);
+    }
 }
